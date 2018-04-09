4fa93a7fc1b0d25ebda13fc1afccc9534ed57644
Rodolfo de Vasconcelos Lopes <rodolfo.lopes@seplag.local>
2018-04-09 16:51:56 -0300

    Banner animations implementations

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, trigger } from '@angular/core';
 
 @Component({
   selector: 'app-acesso',
   templateUrl: './acesso.component.html',
-  styleUrls: ['./acesso.component.css']
+  styleUrls: ['./acesso.component.css'],
+  animations: [
+    trigger('animacao-banner', [])
+  ]
 })
 export class AcessoComponent implements OnInit {
 
--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,27 +1,62 @@
-import { Component, OnInit, trigger,  state, style } from '@angular/core';
+import { Component, OnInit, trigger, state, style, transition, animate } from '@angular/core';
+import { Imagem } from './imagem.model';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.css'],
   animations: [
-    trigger('banner',[
+    trigger('banner', [
       state('escondido', style({
         opacity: 0
-      }) ),
+      })),
       state('visivel', style({
         opacity: 1
-      }))
+      })),
+      transition('escondido <=> visivel', animate('1s ease-in'))
     ])
   ]
 })
 export class BannerComponent implements OnInit {
 
-  public estado:string = 'escondido'
+  public estado: string = 'visivel'
+
+  public imagens: Imagem[] = [
+    { estado: 'visivel', url: '/assets/banner-acesso/img_1.png' },
+    { estado: 'escondido', url: '/assets/banner-acesso/img_2.png' },
+    { estado: 'escondido', url: '/assets/banner-acesso/img_3.png' },
+    { estado: 'escondido', url: '/assets/banner-acesso/img_4.png' },
+    { estado: 'escondido', url: '/assets/banner-acesso/img_5.png' },
+  ]
 
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => this.logicaRotacao(), 2000)
   }
 
+  public logicaRotacao(): void {
+    console.log(this.imagens)
+
+    //ocultar
+    for (let i: number = 0; i <= 4; i++) {
+      if (this.imagens[i].estado === 'visivel') {
+        this.imagens[i].estado = 'escondido'
+
+        if (i < 4) {
+          this.imagens[i + 1].estado = 'visivel'
+        }
+        else {
+          this.imagens[1].estado = 'visivel'
+        }
+
+        break
+      }
+    }
+
+    //exibir a proxima imagem
+
+
+    setTimeout(() => this.logicaRotacao(), 2000)
+  }
 }
